27a729441f0d53cd925a6fc90da07612b177e581
Amy Amin <aa17060@bristol.ac.uk>
2023-01-10 20:43:12 +0000

    working model files

---
+++ plot.py
@@ -0,0 +1,28 @@
+def plot_knn(model, X, y, resolution=100, ax=None):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import pandas as pd
+
+    if X.shape[1] != 2:
+        raise ValueError(f"X must have exactly two features to plot, you passed X.shape={X.shape}")
+    if not isinstance(X, pd.DataFrame):
+        X = pd.DataFrame(X)
+    x1 = X.iloc[:,0]
+    x2 = X.iloc[:,1]
+    x1_range = np.linspace(x1.min()*1.1 - x1.max()*0.1, x1.max()*1.1 - x1.min()*0.1, resolution)
+    x2_range = np.linspace(x2.min()*1.1 - x2.max()*0.1, x2.max()*1.1 - x2.min()*0.1, resolution)
+    grid_x1_values, grid_x2_values = np.meshgrid(x1_range, x2_range)
+    x_prime = np.column_stack((grid_x1_values.ravel(), grid_x2_values.ravel()))
+    x_prime = pd.DataFrame(x_prime, columns=X.columns)
+    y_hat = model.predict(x_prime).reshape(grid_x1_values.shape)
+   
+    cat_map = {c: i for i, c in enumerate(y.unique())}
+    new_y_hat = np.empty_like(y_hat, dtype=int)
+    for k, v in cat_map.items():
+        new_y_hat[y_hat==k] = v
+    new_y = y.replace(cat_map)
+
+    if ax is None:
+        fig, ax = plt.subplots()
+    ax.pcolormesh(grid_x1_values, grid_x2_values, new_y_hat, cmap="Pastel2", alpha=1.0, shading="auto")
+    X.plot.scatter(0, 1, c=new_y, colormap="Dark2", colorbar=False, alpha=0.8, ax=ax)
